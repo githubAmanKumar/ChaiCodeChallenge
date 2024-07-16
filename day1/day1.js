d15a02b846071feaee6d9b7a3229974368c118fc
Aman <140422752+githubAmanKumar@users.noreply.github.com>
2024-07-16 15:54:13 +0530

    Add files via upload

---
+++ day1/day1.js
@@ -0,0 +1,32 @@
+// Activity 1
+var num = 10  ;
+console.log(num);
+let str = "10"  ;
+console.log(str);
+
+// Activity 2
+const fixed = true;
+console.log(fixed);
+
+// Activity 3
+var num = 10  ;
+let str2 = "10"  ;
+const fixed2 = true;
+const obj = {};
+const arr = [];
+console.log(typeof(num));
+console.log(typeof(str2));
+console.log(typeof(fixed2));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+// Activity 4
+let a = "Aman";
+console.log(a);
+a = "kumar"  ;
+console.log(a);
+
+// Activity 5
+const fixed3 = 2;
+fixed3=3;
+// TypeError: Assignment to constant variable.
